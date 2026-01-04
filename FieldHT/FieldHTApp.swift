8eca77327e42688250aea0f9f89ebbbd5025bf89
Benjamin Faershtein <119711889+RCGV1@users.noreply.github.com>
2026-01-04 12:21:58 -0800

    Initial Commit

---
+++ FieldHT/FieldHTApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldHTApp.swift
+//  FieldHT
+//
+//  Created by Benjamin Faershtein on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FieldHTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
